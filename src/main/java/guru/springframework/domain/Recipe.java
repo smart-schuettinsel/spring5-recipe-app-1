829ba5d54cdcf660ff16866103673763adf1de5b
Tomas <tomas.bognar@outlook.com>
2021-01-07 14:24:16 +0100

    - added another Entity's

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -17,8 +17,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -26,8 +24,17 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Catergory> categories;
 
     public Long getId() {
         return id;
@@ -101,12 +108,28 @@ public class Recipe {
         this.image = image;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public Notes getNotes() {
         return notes;
     }
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Catergory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Catergory> catergories) {
+        this.categories = catergories;
     }
 
     public Set<Ingredient> getIngredients() {
